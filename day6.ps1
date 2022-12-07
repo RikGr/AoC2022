9715bae40b4f706491ec49964e6ff7ce7ad02830
Rik Groenewoud <rgroenewoud@xpirit.com>
2022-12-07 14:42:04 +0100

    made it more readable in console

--- day6.ps1
+++ day6.ps1
@@ -5,11 +5,10 @@ DO {
 $makeSet = ($data.Substring(0,4)).ToCharArray() | Sort-Object -Unique
 $data = $data.Substring(1)
 $counter++
-Write-Host $counter
 }
 WHILE ($makeSet.Count -lt 4) 
 
-Write-Host "Marker is $($counter + 3)"
+Write-Host "Marker 1 is $($counter + 3)"
 
 #Question 2 
 $data = get-content -path .\input6.txt
@@ -18,8 +17,7 @@ DO {
 $makeSet = ($data.Substring(0,14)).ToCharArray() | Sort-Object -Unique
 $data = $data.Substring(1)
 $counter++
-Write-Host $counter
 }
 WHILE ($makeSet.Count -lt 14) 
 
-Write-Host "Marker is $($counter + 13)"
+Write-Host "Marker 2 is $($counter + 13)"
