386175852a1ec1e5357ee7f9516bc2de23b9a162
Rik Groenewoud <rgroenewoud@xpirit.com>
2022-12-12 22:23:19 +0100

    day 7 finally completed with help from Casper

--- day7.ps1
+++ day7.ps1
@@ -1,54 +1,111 @@
-$data = get-content -path .\dummy_input7.txt
-#$data = get-content -path .\input7.txt
-$data += "$ cd .."
-$dirs=$null
-$counter = -1
-$cds = $null 
-foreach ($cd in $data){ 
-    $counter = $counter + 1  
-    if ($cd -like "$ cd *" -and $cd -notlike "$ cd .."){
-    $cds += @{($cd+$counter) = $counter}
-    }
+$data = get-content -path .\input7.txt
 
-}
-$cds = $cds.GetEnumerator() | sort -Property Value 
-
-
-foreach ($cd in $cds){
-    $dir = $null
-     $y = $cd.Value  
-     for ($x=$data[$y];$x -ne "$ cd .." ; $x=$data[$y++]){
-
-      $dir += @($data[$y])
-      Write-Host $dir
-         
-    }
-   $dirs += @{$cd.Name = $dir}
+class Directory {
+    [string]$Name
+    [int]$Size
 }
 
-$sumTotal = 0
-foreach ($cd in $cds) {
+[Directory[]]$dirs =  [System.Collections.ArrayList]@()
+[string]$currentDir="/"
 
-    $list = $dirs[$cd.Name]
-    
-    $sizeList = $null
-    foreach ($l in $list) {
-        if ($l -match ".*\d+.*" -eq $true) {
-           $sizeList = @($sizeList + $l)
+foreach ($row in $data)
+{
+    Write-Output($row)
+    if ($row -eq "$ cd /")
+    {
+        $currentDir = "/"
+        if (($dirs | Where-Object {$._Name -eq $currentDir}) -eq $null) # Not adding yet
+        {
+            $directory = New-Object Directory
+            $directory.Name = $currentDir
+            $directory.Size = 0
+            $dirs += $directory
         }
     }
-    $total = 0 
-    foreach ($s in $sizeList){
-        $total += [int]$s.Split(" ")[0]
+    elseif ( $row -eq "$ cd ..")
+    {
+        # currentPath = "/A/B" -> "/A"
+        $lastIndex = $currentDir.LastIndexOf('/')
+        $currentDir = $currentDir.Substring(0, $lastIndex)
+    }
+    elseif ($row -like "$ cd *")
+    {
+        $newdir = $row.Split()[2]
+        if ($currentDir -match '/$') # Doesn't work yet
+        {
+            $currentDir += $newdir
+        }
+        else 
+        {
+            $currentDir += "/" + $newdir
+        }
+
+        if (($dirs | Where-Object {$_.Name -eq $currentDir}) -eq $null) # Not adding yet
+        {
+            $directory = New-Object Directory
+            $directory.Name = $currentDir
+            $directory.Size = 0
+            $dirs += $directory
+        }
+    }
+    elseif ( $row -like "dir *")
+    {
+        if ($currentDir -match '/$')
+        {
+            $newdir = $currentDir + $row.Split()[1]
+        }
+        else 
+        {
+            $newdir = $currentDir + "/" + $row.Split()[1]
+        }
+
+        if (!$dirs | Where-Object {$_.Name -eq $newDir})
+        {
+            $directory = New-Object Directory
+            $directory.Name = $newdir
+            $directory.Size = 0
+            $dirs += $directory
+        }
+    }
+    elseif ($row -match "[0-9]+\ *")
+    {
+        $length = $dirs.length
+        Write-Output "Total length = $length"
         
-
-        }
-     Write-Host "$($cd.Name) = $total"   
-    
-     if ($total -le 100000)  {
-        $sumTotal = $sumTotal + $total
-     } 
-
-     Write-Host "Total below 100000" $sumTotal
-
+        $directory = $dirs | Where-Object {$_.Name -eq $currentDir} 
+        $directory.Size += $row.Split(" ")[0]
     }
+}
+
+$totalSizes= [System.Collections.ArrayList]@()
+foreach ($dir in $dirs){
+  
+    $totalSizeOfDir = (($dirs | where {$_.Name.startsWith($dir.Name)}).Size | Measure-Object -Sum).Sum 
+
+    $totalSizes.Add($totalSizeOfDir)
+}
+
+$totalUnder100K= [System.Collections.ArrayList]@()
+foreach($total in $totalSizes){ 
+    if ($total -le 100000) {
+
+        $totalUnder100K.Add($total)
+    } 
+}
+
+Write-Host ($totalUnder100K | Measure-Object -Sum).Sum 
+
+#question 2: 
+
+$totalUsed = ($dirs.Size | Measure-Object -Sum).Sum
+$currentFree = 70000000 - $totalUsed
+$needed = 30000000 - $currentFree
+
+foreach($size in $totalSizes){
+    $x= $x+1
+  $check += @{ "$size $x" = ($size - $needed) }
+}
+
+$check | Sort-Object $_.Value -Descending
+$check = $check.GetEnumerator() | sort -Property Value  
+Write-Host "Size of dir that can be deleted: $((($check.GetEnumerator() | Where-Object -Property  Value -gt  0)[0].Name).Split(" ")[0])"
---
+++ day7attempt1.ps1
@@ -0,0 +1,52 @@
+$data = get-content -path .\dummy_input7.txt
+#$data = get-content -path .\input7.txt
+$data += "$ cd .."
+$dirs=$null
+$counter = -1
+$cds = $null 
+foreach ($cd in $data){ 
+    $counter = $counter + 1  
+    if ($cd -like "$ cd *" -and $cd -notlike "$ cd .."){
+    $cds += @{($cd+$counter) = $counter}
+    }
+
+}
+$cds = $cds.GetEnumerator() | sort -Property Value 
+
+foreach ($cd in $cds){
+    $dir = $null
+     $y = $cd.Value  
+     for ($x=$data[$y];$x -ne "$ cd .." ; $x=$data[$y++]){
+
+      $dir += @($data[$y])
+      Write-Host $dir
+         
+    }
+   $dirs += @{$cd.Name = $dir}
+}
+
+$sumTotal = 0
+foreach ($cd in $cds) {
+
+    $list = $dirs[$cd.Name]
+    
+    $sizeList = $null
+    foreach ($l in $list) {
+        if ($l -match ".*\d+.*" -eq $true) {
+           $sizeList = @($sizeList + $l)
+        }
+    }
+    $total = 0 
+    foreach ($s in $sizeList){
+        $total += [int]$s.Split(" ")[0]
+        
+        }
+     Write-Host "$($cd.Name) = $total"   
+    
+     if ($total -le 100000)  {
+        $sumTotal = $sumTotal + $total
+     } 
+
+     Write-Host "Total below 100000:" $sumTotal
+
+    }
